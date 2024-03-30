f4c01e1e147ad7b45998edd8880f464866e27837
MBiernat280964 <280964@student.pwr.edu.pl>
2024-03-30 17:42:18 +0000

    introduces control capabilities for the motors

--- src/scorpio_zadanie_rekrutacyjne_software/web/index.js
+++ src/scorpio_zadanie_rekrutacyjne_software/web/index.js
@@ -33,7 +33,7 @@ const motor_0= {
     }),
 
     //variables for motor_0
-    speed :0,
+    valve :0,
     angle :0
 };
 //motor_1
@@ -54,7 +54,7 @@ const motor_1= {
     }),
 
     //variables for motor_0
-    speed :0,
+    valve :0,
     angle :0
 };
 //motor_2
@@ -75,15 +75,39 @@ const motor_2= {
     }),
 
     //variables for motor_0
-    speed :0,
+    valve :0,
     angle :0
 };
 
-//accessing html objects
+//html
 const mAngle_0 = document.getElementById("motorAngle_0");
 const mAngle_1 = document.getElementById("motorAngle_1");
 const mAngle_2 = document.getElementById("motorAngle_2");
+const disMot = document.getElementById("disableMotor");
+
+//motor disable button functionality
+dis_option = "on";
+disMot.onclick = function(){
+    if(dis_option=="on"){
+        dis_option="off";
+        console.log('disabled');
+    }
+    else{
+        dis_option="on";
+        console.log('enabled');
+    }
+}
 //updates position of motors
+
+function move_motor(motor, dir){
+    let x = dir*13;
+    motor.valve = x;
+    motor.pub.publish(new ROSLIB.Message({
+        data : x
+    }))
+    console.log('speed: '+x);
+};
+
 function update_pos(){
     motor_0.lis.removeAllListeners();
     motor_0.lis.subscribe(function(message) {
@@ -100,10 +124,47 @@ motor_2.lis.subscribe(function(message) {
 motor_2.angle = Math.ceil((message.data * 360) / 4095);
 motor_2.lis.unsubscribe();
 });
-//console.log(motor_0.angle +'  motor_0');
-mAngle_0.textContent = motor_0.angle;
-mAngle_1.textContent = motor_1.angle;
-mAngle_2.textContent = motor_2.angle;
+mAngle_0.textContent = motor_0.angle +'  stopni';
+mAngle_1.textContent = motor_1.angle +'  stopni';
+mAngle_2.textContent = motor_2.angle +'  stopni';
 };
 
-const timer = setInterval(update_pos, 100);
+const timer = setInterval(update_pos, 500);
+
+//key_pressed handler
+
+document.addEventListener("keypress", event => {
+    console.log(event);
+    if(dis_option=="on"){
+        switch(event.key){
+            case "q":
+                move_motor(motor_0, -1);
+                break;
+            case "e":
+                move_motor(motor_0, 1);
+                break;
+            case "a":
+                move_motor(motor_1, -1);
+                break;
+            case "d":
+                move_motor(motor_1, 1);
+                break;
+            case "z":
+                move_motor(motor_2, -1);
+                break;
+            case "c":
+                move_motor(motor_2, 1);
+                break;
+        }
+    }
+    else console.log('motor functions disabled!');
+    /*setTimeout(() => {
+        move_motor(motor_0,0);
+        move_motor(motor_1,0);
+        move_motor(motor_2,0);
+      }, 1000);*/
+      
+});
+
+
+
