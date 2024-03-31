e8134e3d09a8641fccdc8900b74697a01bece442
MBiernat280964 <280964@student.pwr.edu.pl>
2024-03-31 00:17:04 +0000

    adds motor power sliders, improves on GUI

--- src/scorpio_zadanie_rekrutacyjne_software/web/index.js
+++ src/scorpio_zadanie_rekrutacyjne_software/web/index.js
@@ -33,49 +33,49 @@ const motor_0= {
     }),
 
     //variables for motor_0
-    valve :0,
+    power :0,
     angle :0
 };
 //motor_1
 const motor_1= {
 
-    //publisher for motor_0
+    //publisher for motor_1
     pub : new ROSLIB.Topic({
     ros : ros,
     name : '/virtual_dc_motor_node/set_cs_1',
     messageType : 'std_msgs/Int8'
     }),
 
-    //listener for motor_0
+    //listener for motor_1
     lis : new ROSLIB.Topic({
     ros : ros,
     name : '/virtual_dc_motor_node/get_position_1',
     messageType : 'std_msgs/UInt16'
     }),
 
-    //variables for motor_0
-    valve :0,
+    //variables for motor_1
+    power :0,
     angle :0
 };
 //motor_2
 const motor_2= {
 
-    //publisher for motor_0
+    //publisher for motor_2
     pub : new ROSLIB.Topic({
     ros : ros,
     name : '/virtual_dc_motor_node/set_cs_2',
     messageType : 'std_msgs/Int8'
     }),
 
-    //listener for motor_0
+    //listener for motor_2
     lis : new ROSLIB.Topic({
     ros : ros,
     name : '/virtual_dc_motor_node/get_position_2',
     messageType : 'std_msgs/UInt16'
     }),
 
-    //variables for motor_0
-    valve :0,
+    //variables for motor_2
+    power :0,
     angle :0
 };
 
@@ -84,24 +84,41 @@ const mAngle_0 = document.getElementById("motorAngle_0");
 const mAngle_1 = document.getElementById("motorAngle_1");
 const mAngle_2 = document.getElementById("motorAngle_2");
 const disMot = document.getElementById("disableMotor");
+const slider_0 = document.getElementById("myRange_0");
+const slider_1 = document.getElementById("myRange_1");
+const slider_2 = document.getElementById("myRange_2");
+
+// update current slider value
+slider_0.oninput = function() {
+  console.log(this.value);
+  //move_motor(motor_0,this.value);
+}
+slider_1.oninput = function() {
+    console.log(this.value+' motor 1');
+    //move_motor(motor_1,this.value);
+}
+slider_2.oninput = function() {
+    console.log(this.value+' motor 2');
+    //move_motor(motor_2,this.value);
+}
 
 //motor disable button functionality
-dis_option = "on";
+disMot.textContent="Disable motor control"
 disMot.onclick = function(){
-    if(dis_option=="on"){
-        dis_option="off";
+    if(this.textContent=="Disable motor control"){
+        this.textContent="Enable motor control"
         console.log('disabled');
     }
     else{
-        dis_option="on";
+        this.textContent="Disable motor control"
         console.log('enabled');
     }
 }
 //updates position of motors
 
 function move_motor(motor, dir){
-    let x = dir*13;
-    motor.valve = x;
+    let x = dir;
+    motor.power = x;
     motor.pub.publish(new ROSLIB.Message({
         data : x
     }))
@@ -133,37 +150,57 @@ const timer = setInterval(update_pos, 500);
 
 //key_pressed handler
 
-document.addEventListener("keypress", event => {
+document.addEventListener("keydown", event => {
     console.log(event);
-    if(dis_option=="on"){
+    if(disMot.textContent=="Disable motor control"){
         switch(event.key){
             case "q":
-                move_motor(motor_0, -1);
+                move_motor(motor_0, -1*slider_0.value);
                 break;
             case "e":
-                move_motor(motor_0, 1);
+                move_motor(motor_0, 1*slider_0.value);
                 break;
             case "a":
-                move_motor(motor_1, -1);
+                move_motor(motor_1, -1*slider_1.value);
                 break;
             case "d":
-                move_motor(motor_1, 1);
+                move_motor(motor_1, 1*slider_1.value);
                 break;
             case "z":
-                move_motor(motor_2, -1);
+                move_motor(motor_2, -1*slider_2.value);
                 break;
             case "c":
-                move_motor(motor_2, 1);
+                move_motor(motor_2, 1*slider_2.value);
                 break;
         }
     }
     else console.log('motor functions disabled!');
-    /*setTimeout(() => {
-        move_motor(motor_0,0);
-        move_motor(motor_1,0);
-        move_motor(motor_2,0);
-      }, 1000);*/
-      
+});
+
+document.addEventListener("keyup", event => {
+    if(disMot.textContent=="Disable motor control"){
+        switch(event.key){
+            case "q":
+                move_motor(motor_0, 0);
+                break;
+            case "e":
+                move_motor(motor_0, 0);
+                break;
+            case "a":
+                move_motor(motor_1, 0);
+                break;
+            case "d":
+                move_motor(motor_1, 0);
+                break;
+            case "z":
+                move_motor(motor_2, 0);
+                break;
+            case "c":
+                move_motor(motor_2, 0);
+                break;
+        }
+    }
+    else console.log('controls disabled!');
 });
 
 
